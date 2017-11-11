b9329877e088c474442cccd3c06391e9d90a4f0e
hilljacob <hillboy14@gmail.com>
2017-11-11 15:45:34 -0700

    jh update footer nav

--- directives/header/header-controller.js
+++ directives/header/header-controller.js
@@ -1,18 +1,21 @@
 angular.module('rental').controller('headerCtrl', function($scope){
 
+  
 
-  $(".mobile-open").on("click", function(){
-        $(".mobile-nav").css("right", "0");
-        $(".mobile-nav").css("visibility", "visible");
-        $(".middleNav").css("margin-left", "590px");
-        $(".middleNav").css("visibility", "hidden");
+  $(document).ready(function(){
+    console.log("ready");
+    $(".mobile-open").on("click", function(){
+          $(".mobile-nav").css("right", "0");
+          $(".mobile-nav").css("visibility", "visible");
+          $(".middleNav").css("margin-left", "590px");
+          $(".middleNav").css("visibility", "hidden");
+    });
+    $(".mobile-close").on("click", function(){
+          $(".mobile-nav").css("right", "-250px");
+          $(".mobile-nav").css("visibility", "hidden");
+          $(".middleNav").css("margin-left", "0");
+          $(".middleNav").css("visibility", "visible");
+    });
   });
-  $(".mobile-close").on("click", function(){
-        $(".mobile-nav").css("right", "-250px");
-        $(".mobile-nav").css("visibility", "hidden");
-        $(".middleNav").css("margin-left", "0");
-        $(".middleNav").css("visibility", "visible");
-  });
-
 
 })
